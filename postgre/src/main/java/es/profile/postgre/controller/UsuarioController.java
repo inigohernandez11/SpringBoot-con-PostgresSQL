3e2661b9603383fb5ef2d790e35c7d28a4864e69
inigohernandez11 <i.her.11@hotmail.com>
2023-02-21 13:00:49 +0100

    Commmit con OpenApi

--- postgre/src/main/java/es/profile/postgre/controller/UsuarioController.java
+++ postgre/src/main/java/es/profile/postgre/controller/UsuarioController.java
@@ -4,6 +4,12 @@ import es.profile.postgre.dto.UsuarioCreateDto;
 import es.profile.postgre.dto.UsuarioDto;
 import es.profile.postgre.repository.models.UsuarioRepository;
 import es.profile.postgre.service.UsuarioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
@@ -16,7 +22,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -26,21 +34,43 @@ import java.util.Objects;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/usuarios")
+@RestControllerAdvice
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
     MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
+
+    @Operation(summary = "Devuelve una lista de usuarios con sus atributos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de usuarios devuelta",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = UsuarioDto.class)))
+                    })
+            })
     @GetMapping
     public ResponseEntity<List<UsuarioDto>> getUsuarios() {
-
         return new ResponseEntity<>(usuarioService.findAll(), HttpStatus.OK);
     }
 
+    @Operation(summary = "Crea un usuario y lo introduce en la base de datos de Postgres")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Usuario Creado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UsuarioDto.class)) }) })
     @PostMapping
     public ResponseEntity<UsuarioDto> insertUsuario(@RequestBody UsuarioCreateDto usuario) {
         return new ResponseEntity<>(usuarioService.insert(usuario),HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Devuelve el usuario al que pertenece el id que se pasa por parámetro o advierte de que ese usuario no se encuentra en la base de datos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UsuarioDto.class)) }),
+            @ApiResponse(responseCode = "404", description = "Usuario Not found",
+                    content = @Content) })
     @GetMapping("/{id}")
     public ResponseEntity<UsuarioDto> getUsuario(@PathVariable Integer id)
     {
@@ -51,6 +81,10 @@ public class UsuarioController {
         return new ResponseEntity<>(usuario,HttpStatus.OK);
     }
 
+    @Operation(summary = "Elimina de la base de datos el usuario que se ha pasado por parámetro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Usuario Not found",
+                    content = @Content) })
     @DeleteMapping("/{id}")
     public void deleteUsuario(@PathVariable Integer id)
     {
